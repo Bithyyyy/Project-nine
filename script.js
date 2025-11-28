8e139133d047e3965487e1983a33b12e3b096b6e
Bithyyyy <nusratbithy4386@gmail.com>
2025-11-28 23:45:44 +0600

    Second commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("loginButton").addEventListener("click",function(e){
+    e.preventDefault()
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+
+    const mobileNumberInt = parseInt(document.getElementById("mobile-number").value)
+    const pinNumberInt = parseInt(document.getElementById("pin-number").value)
+    
+    if(pinNumber == pinNumberInt && mobileNumber == mobileNumberInt)
+    {
+        window.location.href ="./home.html"
+    }
+    else{
+        alert("Invalid Credentials!")
+    }
+
+
+})
